13cffa37640e92a926430fbaf711265ad766bbff
swathi <swathichandrasekar226@gmail.com>
2025-12-26 18:44:00 +0530

    improved ui 3 dark bg after img

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,45 +2,29 @@ import profileImg from "../assets/profile.jpeg";
 
 function Hero() {
   return (
-    <section className="hero-section">
-      <div className="container-fluid">
-        <div className="row align-items-center px-lg-5">
+    <section className="container py-5">
+      <div className="row align-items-center">
 
-          {/* IMAGE */}
-          <div className="col-lg-5 text-center mb-4 mb-lg-0">
-            <img
-              src={profileImg}
-              alt="Profile"
-              className="profile-img floating"
-            />
-          </div>
-
-          {/* CONTENT */}
-          <div className="col-lg-7 text-light">
-            <h1 className="fw-bold display-5">Hi, I’m Swathi</h1>
-
-            <p className="fs-5 text-info">
-              React & Full Stack Developer
-            </p>
-
-            <p className="hero-desc">
-              I build clean, responsive web applications using modern
-              technologies with a strong focus on usability and performance.
-            </p>
-
-            <div className="d-flex gap-3 flex-wrap mt-3">
-              <a
-                href="https://www.linkedin.com/"
-                target="_blank"
-                rel="noreferrer"
-                className="btn btn-outline-info"
-              >
-                LinkedIn
-              </a>
-            </div>
-          </div>
-
+        <div className="col-lg-5 text-center">
+          <img
+            src={profileImg}
+            alt="Profile"
+            className="profile-img floating"
+          />
         </div>
+
+        <div className="col-lg-7">
+          <h1>Hi, I’m Swathi</h1>
+          <p className="text-muted">React & Full Stack Developer</p>
+
+          <a
+          href="https://www.linkedin.com/"
+          target="_blank"
+          rel="noreferrer"
+          className="btn btn-outline-primary"
+        ></a>
+        </div>
+
       </div>
     </section>
   );
